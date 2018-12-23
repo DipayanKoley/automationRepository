63b76e1ca3d67ad93c3a7a1d4ef68772fa082216
DipayanKoley <dipayankoley@gmail.com>
2018-12-23 20:52:14 +0530

    Adding new project

---
+++ src/main/java/automationRepositoryFramework/TestCase.java
@@ -0,0 +1,13 @@
+package automationRepositoryFramework;
+
+import org.openqa.selenium.WebDriver;
+
+public class TestCase
+{
+	public static WebDriver driver = null;
+	
+	public static void main(String[] args)
+	{
+		System.out.println("Hello");
+	}
+}
